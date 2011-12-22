7796a441b8cf0946cfb6823b50d3322fe678fa69
Ariel Narvaez <a.e.narvaez.salazar@tue.nl>
2011-12-22 17:45:45 +0100

    add kozeny-carman plot

--- data-figs/plot.sh
+++ data-figs/plot.sh
@@ -9,22 +9,27 @@ gnuplot temp.gp
 
 latex \${file}.tex
 dvips \${file}.dvi
-ps2pdf \${file}.ps
+#sed -i 's/^%%BoundingBox: 0 0 241 236.*$/%%BoundingBox: 0 10 241 189/' \${file}.ps
+ps2eps \${file}.ps
+epstopdf \${file}.eps
+#ps2pdf \${file}.ps
 
-for i in .tex .log .aux .dvi -inc.eps .ps
+for i in .tex .log .aux .dvi -inc.eps
+# .ps
 do
 rm \${file}\${i}
 done
 -eof1-
 chmod +x temp.sh
 
-head="set term epslatex color standalone rounded size 8.5cm, 6.3cm lw 1.0 9.0"
+head2d="set term epslatex color standalone rounded size 8.5cm, 6.3cm lw 1.0 9.0"
+head3d="set term epslatex color standalone rounded size 8.5cm, 8.3cm lw 1.0 9.0 header '\\usepackage{rotating}'"
 margin2d="set tmargin 3; set bmargin 3.5; set lmargin 10.5; set rmargin 2"
-margin3d="set tmargin 3; set bmargin 3.5; set lmargin 6.5; set rmargin 2"
+margin3d="set lmargin 4; set rmargin 0"
 
 plot=porosity
 cat > temp.gp <<- -eof1-
-$head
+$head3d
 set out "${plot}.tex"
 
 $margin3d
@@ -35,82 +40,25 @@ set xtics 0.2 offset 0,-0.25
 set format y "\\\\tiny{%2.1f}"
 set ytics 0.2 offset 1.5,0.0
 
-set format z "\\\\tiny{%3.2f}"
+set format z "\\\\tiny{%2.2f}"
 set ztics 0.02
 unset colorbox
 set ticslevel 0
 
 set xlabel "\$\\\\zeta\$" offset 0,-0.5
 set ylabel "\$\\\\eta\$" offset 0.5,0
-set zlabel "\$\\\\phi\$" offset -1.5,0
-#set title 'Porosity'
+set zlabel "\\\\begin{turn}{90}\$\\\\phi\$\\\\end{turn}" offset -1.5,0
+unset title
 
 splot \
- 'original_orientation_full/data.dat' u 1:2:(1-\$3) notitle w pm3d
--eof1-
-./temp.sh ${plot}
-
-plot=old_porosity
-cat > temp.gp <<- -eof1-
-$head
-set out "${plot}.tex"
-
-$margin3d
-
-set format x "\\\\tiny{%2.1f}"
-set xtics 0.2 offset 0,-0.25
-
-set format y "\\\\tiny{%2.1f}"
-set ytics 0.2 offset 1.5,0.0
-
-set format z "\\\\tiny{%3.2f}"
-set ztics 0.02
-unset colorbox
-set ticslevel 0
-
-set xlabel "\$\\\\zeta\$" offset 0,-0.5
-set ylabel "\$\\\\eta\$" offset 0.5,0
-set zlabel "\$\\\\phi\$" offset -1.5,0
-#set title 'Porosity'
-
-splot \
- 'original_orientation_full/density.dat' u 1:2:(1-\$3) notitle w pm3d
+ 'z-permeability/data.dat' u 1:2:(1-\$3) notitle w pm3d
 -eof1-
 ./temp.sh ${plot}
 
 
 plot=permeability_z
 cat > temp.gp <<- -eof1-
-$head
-set out "${plot}.tex"
-
-$margin3d
-
-set format x "\\\\tiny{%2.1f}"
-set xtics 0.2 offset 0,-0.25
-
-set format y "\\\\tiny{%2.1f}"
-set ytics 0.2 offset 1.5,0.0
-
-set format z "\\\\tiny{%3.2f}"
-set ztics 0.5
-unset colorbox
-set ticslevel 0
-
-set xlabel "\$\\\\zeta\$" offset 0,-0.5
-set ylabel "\$\\\\eta\$" offset 0.5,0
-set zlabel "\$\\\\kappa_{z}\\\\,[10^{6}]\$" offset -1.5,0
-#set title 'Permeability in \$z\$-depth'
-
-splot \
- 'original_orientation_full/data.dat' u 1:2:(\$7*1e6) notitle w pm3d
--eof1-
-./temp.sh ${plot}
-
-
-plot=permeability_x
-cat > temp.gp <<- -eof1-
-$head
+$head3d
 set out "${plot}.tex"
 
 $margin3d
@@ -128,18 +76,45 @@ set ticslevel 0
 
 set xlabel "\$\\\\zeta\$" offset 0,-0.5
 set ylabel "\$\\\\eta\$" offset 0.5,0
-set zlabel "\$\\\\kappa_{x}\\\\,[10^{6}]\$" offset -1.5,0
-#set title 'Permeability \$x\$-depth'
+set zlabel "\\\\begin{turn}{90} \$\\\\kappa_{z}\\\\,[10^{6}]\$ \\\\end{turn}" offset -1.5,0
 
 splot \
- 'rotated_orientation_full/data.dat' u 1:2:(\$7*1e6) notitle w pm3d
+ 'z-permeability/data.dat' u 1:2:(\$7*1e6) notitle w pm3d
+-eof1-
+./temp.sh ${plot}
+
+
+plot=permeability_x
+cat > temp.gp <<- -eof1-
+$head3d
+set out "${plot}.tex"
+
+$margin3d
+
+set format x "\\\\tiny{%2.1f}"
+set xtics 0.2 offset 0,-0.25
+
+set format y "\\\\tiny{%2.1f}"
+set ytics 0.2 offset 1.5,0.0
+
+set format z "\\\\tiny{%2.1f}"
+set ztics 0.5
+unset colorbox
+set ticslevel 0
+
+set xlabel "\$\\\\zeta\$" offset 0,-0.5
+set ylabel "\$\\\\eta\$" offset 0.5,0
+set zlabel "\\\\begin{turn}{90} \$\\\\kappa_{x}\\\\,[10^{6}]\$ \\\\end{turn}" offset -1.5,0
+
+splot \
+ 'x-permeability/data.dat' u 1:2:(\$7*1e6) notitle w pm3d
 -eof1-
 ./temp.sh ${plot}
 
 
 plot=permeability_ratio
 cat > temp.gp <<- -eof1-
-$head
+$head3d
 set out "${plot}.tex"
 
 $margin3d
@@ -157,18 +132,17 @@ set ticslevel 0
 
 set xlabel "\$\\\\zeta\$" offset 0,-0.5
 set ylabel "\$\\\\eta\$" offset 0.5,0
-set zlabel "\$\\\\kappa_{z}/\\\\kappa_{x}\$" offset -1.5,0
-#set title 'Permeability Ratio'
+set zlabel "\\\\begin{turn}{90} \$\\\\kappa_{z}/\\\\kappa_{x}\$ \\\\end{turn}" offset -1.5,0
 
 splot \
- '< paste original_orientation_full/data.dat rotated_orientation_full/data.dat' u 1:2:(\$7/\$14) notitle w pm3d
+ '< paste z-permeability/data.dat x-permeability/data.dat' u 1:2:(\$7/\$14) notitle w pm3d
 -eof1-
 ./temp.sh ${plot}
 
 
 plot=lc_z
 cat > temp.gp <<- -eof1-
-$head
+$head3d
 set out "${plot}.tex"
 
 $margin3d
@@ -186,8 +160,7 @@ set ticslevel 0
 
 set xlabel "\$\\\\zeta\$" offset 0,-0.5
 set ylabel "\$\\\\eta\$" offset 0.5,0
-set zlabel "\$l_{c}\$" offset -1.5,0
-#set title 'Carachteristic Length in \$z\$-depth'
+set zlabel "\\\\begin{turn}{90} \$l_{c}\$ \\\\end{turn}" offset -1.5,0
 
 splot \
  'lc_z/lc_512.asc' u 1:2:3 notitle w pm3d
@@ -197,7 +170,7 @@ splot \
 
 plot=lc_x
 cat > temp.gp <<- -eof1-
-$head
+$head3d
 set out "${plot}.tex"
 
 $margin3d
@@ -215,8 +188,7 @@ set ticslevel 0
 
 set xlabel "\$\\\\zeta\$" offset 0,-0.5
 set ylabel "\$\\\\eta\$" offset 0.5,0
-set zlabel "\$l_{c}\$" offset -1.5,0
-#set title 'Characteristic Length in \$x\$-depth'
+set zlabel "\\\\begin{turn}{90} \$l_{c}\$ \\\\end{turn}" offset -1.5,0
 
 splot \
  'lc_x/lc_512.2.asc' u 1:2:3 notitle w pm3d
@@ -226,7 +198,7 @@ splot \
 
 plot=k_lc_z
 cat > temp.gp <<- -eof1-
-$head
+$head3d
 set out "${plot}.tex"
 
 $margin3d
@@ -244,18 +216,17 @@ set ticslevel 0
 
 set xlabel "\$\\\\zeta\$" offset 0,-0.5
 set ylabel "\$\\\\eta\$" offset 0.5,0
-set zlabel "\$\\\\kappa/{l_{c}}^{2}\$" offset -1.5,0
-#set title 'Permeability normalized by \$l_{c}\$ in \$z\$-depth'
+set zlabel "\\\\begin{turn}{90} \$\\\\kappa/{l_{c}}^{2}\$ \\\\end{turn}" offset -1.5,0
 
 splot \
- '< paste original_orientation_full/data.dat lc_z/lc_512.asc' u 1:2:(\$7/(\$10)**2) notitle w pm3d
+ '< paste z-permeability/data.dat lc_z/lc_512.asc' u 1:2:(\$7/(\$10)**2) notitle w pm3d
 -eof1-
 ./temp.sh ${plot}
 
 
 plot=k_lc_x
 cat > temp.gp <<- -eof1-
-$head
+$head3d
 set out "${plot}.tex"
 
 $margin3d
@@ -274,17 +245,16 @@ set ticslevel 0
 set xlabel "\$\\\\zeta\$" offset 0,-0.5
 set ylabel "\$\\\\eta\$" offset 0.5,0
 set zlabel "\$\\\\kappa/{l_{c}}^{2}\$" offset -1.5,0
-#set title 'Permeability normalized  by \$l_{c}\$ in \$x\$-depth'
 
 splot \
- '< paste rotated_orientation_full/data.dat lc_x/lc_512.2.asc' u 1:2:(\$7/(\$10)**2) notitle w pm3d
+ '< paste x-permeability/data.dat lc_x/lc_512.2.asc' u 1:2:(\$7/(\$10)**2) notitle w pm3d
 -eof1-
 ./temp.sh ${plot}
 
 
 plot=phi_k_lc_z
 cat > temp.gp <<- -eof1-
-$head
+$head2d
 set out "${plot}.tex"
 
 $margin2d
@@ -299,17 +269,16 @@ set ytics 0.005
 
 set xlabel "\$\\\\phi\$"
 set ylabel "\$ \\\\kappa/{l_{c}}^{2}\$"
-#set title "\$\\\\kappa\$ normalized by \$l_{c}\$ in \$z\$-depth"
 
 plot \
- '< paste original_orientation_full/data.dat lc_z/lc_512.asc' u (1-\$3):(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
+ '< paste z-permeability/data.dat lc_z/lc_512.asc' u (1-\$3):(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
 -eof1-
 ./temp.sh ${plot}
 
 
-plot=eta_k_lc_z
+plot=zeta_k_lc_z
 cat > temp.gp <<- -eof1-
-$head
+$head2d
 set out "${plot}.tex"
 
 $margin2d
@@ -324,17 +293,16 @@ set ytics 0.005
 
 set xlabel "\$\\\\zeta\$"
 set ylabel "\$ \\\\kappa/{l_{c}}^{2}\$"
-#set title "\$\\\\kappa\$ normalized by \$l_{c}\$ in \$z\$-depth"
 
 plot \
- '< paste original_orientation_full/data.dat lc_z/lc_512.asc' u 1:(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
+ '< paste z-permeability/data.dat lc_z/lc_512.asc' u 1:(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
 -eof1-
 ./temp.sh ${plot}
 
 
 plot=xi_k_lc_z
 cat > temp.gp <<- -eof1-
-$head
+$head2d
 set out "${plot}.tex"
 
 $margin2d
@@ -349,17 +317,16 @@ set ytics 0.005
 
 set xlabel "\$\\\\eta\$"
 set ylabel "\$ \\\\kappa/{l_{c}}^{2}\$"
-#set title "\$\\\\kappa\$ normalized by \$l_{c}\$ in \$z\$-depth"
 
 plot \
- '< paste original_orientation_full/data.dat lc_z/lc_512.asc' u 2:(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
+ '< paste z-permeability/data.dat lc_z/lc_512.asc' u 2:(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
 -eof1-
 ./temp.sh ${plot}
 
 
 plot=phi_k_lc_x
 cat > temp.gp <<- -eof1-
-$head
+$head2d
 set out "${plot}.tex"
 
 $margin2d
@@ -374,17 +341,16 @@ $margin2d
 
 set xlabel "\$\\\\phi\$"
 set ylabel "\$ \\\\kappa/{l_{c}}^{2}\$"
-#set title "\$\\\\kappa\$ normalized by \$l_{c}\$ in \$\x\$-depth"
 
 plot \
- '< paste rotated_orientation_full/data.dat lc_x/lc_512.asc' u (1-\$3):(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
+ '< paste x-permeability/data.dat lc_x/lc_512.2.asc' u (1-\$3):(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
 -eof1-
 ./temp.sh ${plot}
 
 
-plot=eta_k_lc_x
+plot=zeta_k_lc_x
 cat > temp.gp <<- -eof1-
-$head
+$head2d
 set out "${plot}.tex"
 
 $margin2d
@@ -399,17 +365,16 @@ set ytics 0.005
 
 set xlabel "\$\\\\zeta\$"
 set ylabel "\$ \\\\kappa/{l_{c}}^{2}\$"
-#set title "\$\\\\kappa\$ normalized by \$l_{c}\$ in \$x\$-depth"
 
 plot \
- '< paste rotated_orientation_full/data.dat lc_x/lc_512.asc' u 1:(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
+ '< paste x-permeability/data.dat lc_x/lc_512.2.asc' u 1:(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
 -eof1-
 ./temp.sh ${plot}
 
 
 plot=xi_k_lc_x
 cat > temp.gp <<- -eof1-
-$head
+$head2d
 set out "${plot}.tex"
 
 $margin2d
@@ -424,13 +389,44 @@ set ytics 0.005
 
 set xlabel "\$\\\\eta\$"
 set ylabel "\$ \\\\kappa/{l_{c}}^{2}\$"
-#set title "\$\\\\kappa\$ normalized by \$l_{c}\$ in \$x\$-depth"
 
 plot \
- '< paste rotated_orientation_full/data.dat lc_x/lc_512.asc' u 2:(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
+ '< paste x-permeability/data.dat lc_x/lc_512.2.asc' u 2:(\$7/(\$10)**2) notitle pt 6 ps 2 lw 2
 -eof1-
 ./temp.sh ${plot}
 
+
+
+plot=kozeny_carman
+cat > temp.gp <<- -eof1-
+$head3d
+set out "${plot}.tex"
+
+$margin3d
+
+set format x "\\\\tiny{%2.1f}"
+set xtics 0.2 offset 0,-0.25
+
+set format y "\\\\tiny{%2.1f}"
+set ytics 0.2 offset 1.5,0.0
+
+set format z "\\\\tiny{%2.1f}"
+set ztics 0.5
+unset colorbox
+set ticslevel 0
+
+set xlabel "\$\\\\zeta\$" offset 0,-0.5
+set ylabel "\$\\\\eta\$" offset 0.5,0
+set zlabel "\\\\begin{turn}{90} \$\\\\kappa\\\\,[10^{6}]\$ \\\\end{turn}" offset -1.5,0
+
+D(x,y,z)=(x*y*z)**(1./3.)
+sphericity(x,y,z)= D(x,y,z)**2 / ( ( (x*y)**p + (x*z)**p + (y*z)**p )/3.0 )**(1.0/p)
+p=1.6075
+
+splot \
+ '< paste ellips.dat z-permeability/data.dat' u 1:2:(sphericity(\$3,\$4,\$5)**2*(2.0*D(\$3,\$4,\$5))**2/150.0*(1-\$8)**3/(\$8)**2*1e6) notitle w pm3d
+-eof1-
+./temp.sh ${plot}
 
 rm temp.sh temp.gp
 
